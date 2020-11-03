d488a0cf6dea11d4324014d922f11e3bbe6f9062
p <p>
2020-11-02 21:31:40 -0800

    periodic commit

--- config.h
+++ config.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "sourcecodepro-regular:pixelsize=14:antialias=true:autohint=true";
+static char *font = "TerminessTTF Nerd Font Mono:style=medium:pixelsize=16:antialias=true:autohint=true";
 static int borderpx = 2;
 
 /*
