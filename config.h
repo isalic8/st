bbb19abffb418cdafa3bece07b14cb1c500073d2
p <p>
2021-02-07 21:45:05 -0800

    periodic commit

--- config.h
+++ config.h
@@ -85,29 +85,28 @@ unsigned int tabspaces = 4;
 
 /* Terminal colors (16 first used in escape sequence) */
 static const char *colorname[] = {
+  [0] = "#282A36", /* black   */
+  [1] = "#ff5555", /* red     */
+  [2] = "#50fa7b", /* green   */
+  [3] = "#f1fa8c", /* yellow  */
+  [4] = "#bd93f9", /* blue    */
+  [5] = "#ff79c6", /* magenta */
+  [6] = "#8be9fd", /* cyan    */
+  [7] = "#bfbfbf", /* white   */
 
-	"#000000", /*hard contrast: #1d2021 / soft contrast: #32302f*/
-	"#cc241d",
-	"#98971a",
-	"#d79921",
-	"#458588",
-	"#b16286",
-	"#689d6a",
-	"#a89984",
-	"#928374",
-	"#fb4934",
-	"#b8bb26",
-	"#fabd2f",
-	"#83a598",
-	"#d3869b",
-	"#8ec07c",
-	"#ebdbb2",
-	[255] = 0,
-	"#add8e6", /*256 -> cursor*/
-	"#555555", /*257 -> rev cursor*/
-	/*"#282828", 258 -> bg*/
-	"#000000", /*258 -> bg*/
-	"#ebdbb2", /*259 -> fg*/
+  /* 8 bright colors */
+  [8]  = "#4d4d4d", /* black   */
+  [9]  = "#ff6e67", /* red     */
+  [10] = "#5af78e", /* green   */
+  [11] = "#f4f99d", /* yellow  */
+  [12] = "#caa9fa", /* blue    */
+  [13] = "#ff92d0", /* magenta */
+  [14] = "#9aedfe", /* cyan    */
+  [15] = "#e6e6e6", /* white   */
+
+  /* special colors */
+  [256] = "#282a36", /* background */
+  [257] = "#f8f8f2", /* foreground */
 };
 
 
